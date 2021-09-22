062b74255552acd81d843f47db554c069a705722
kbenya <kbb1795@gmail.com>
2021-09-22 13:55:58 +0200

    subset creation step 1

--- src/create_subsets.py
+++ src/create_subsets.py
@@ -1,3 +1,13 @@
-def create_subsets(subset_name, **kwargs):
+from sscutils import dump_dfs_to_trepos
+
+from .trepos import credits_table, details_table, micro_table, users_table, ratings_table
+
+def create_subsets(subset_name, num_bg_ratings = 10):
     """create subsets that are described in the config of the repo"""
-    pass
+    credits_df = credits_table.get_full_df()
+    details_df = details_table.get_full_df()
+    micro_df = micro_table.get_full_df()
+    users_df = users_table.get_full_df()
+    ratings_df = ratings_table.get_full_df().iloc[:num_bg_ratings, :]
+
+    dump_dfs_to_trepos(subset_name, [(credits_df, credits_table), (details_df, details_table), (micro_df, micro_table), (users_df, users_table), (ratings_df, ratings_table)])
--- src/trepos.py
+++ src/trepos.py
@@ -1,3 +1,7 @@
 from sscutils import create_trepo_with_subsets
 
-objects_table = create_trepo_with_subsets("objects")
+credits_table = create_trepo_with_subsets("credits")
+details_table = create_trepo_with_subsets("details")
+ratings_table = create_trepo_with_subsets("ratings")
+micro_table = create_trepo_with_subsets("micro")
+users_table = create_trepo_with_subsets("users")
